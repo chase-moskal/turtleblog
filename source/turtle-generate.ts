3d34cae11611332d91c8fe43b84e77e5d9ed838f
Chase Moskal <chasemoskal@gmail.com>
2018-10-08 17:35:46 -0700

    improve commenting in turtle-generate

--- source/turtle-generate.ts
+++ source/turtle-generate.ts
@@ -21,64 +21,76 @@ export const turtleGenerate: TurtleGenerator = async({websiteMetadata}) => {
 		return page
 	}
 
+	//
+	// obtain page data
+	//
+
 	const home = getPageById(websiteMetadata.references.home)
 	const blogIndex = getPageById(websiteMetadata.references.blogIndex)
 	const blogPosts = websiteMetadata.references.blogPosts.map(getPageById)
 	const articles = websiteMetadata.references.articles.map(getPageById)
 
-	const pages = [
-
-		// home page
-		{
-			id: home.id,
-			name: "home",
-			distPath: "index.html",
-			content: "",
-			files: home.files.map(file => ({
-				fullSourceFilePath: `${source}/${home.sourcePath}/${file}`,
-				distDirPath: ""
-			}))
-		},
-
-		// blog index
-		{
-			id: blogIndex.id,
-			name: "blog-index",
-			distPath: "blog/index.html",
-			content: "",
-			files: blogIndex.files.map(file => ({
-				fullSourceFilePath: `${source}/${home.sourcePath}/${file}`,
-				distDirPath: ""
-			}))
-		},
-
-		// blog posts
-		...blogPosts.map(page => ({
-			id: page.id,
-			name: path.basename(page.sourcePath),
-			distPath: page.sourcePath.replace(/^blog\//i, "") + "/index.html",
-			content: "",
-			files: page.files.map(filename => ({
-				fullSourceFilePath: `${source}/${page.sourcePath}/${filename}`,
-				distDirPath: page.sourcePath
-			}))
-		})),
-
-		// articles
-		...articles.map(page => ({
-			id: page.id,
-			name: path.basename(page.sourcePath),
-			distPath: page.sourcePath.replace(/^articles\//i, "") + "/index.html",
-			content: "",
-			files: page.files.map(filename => ({
-				fullSourceFilePath: `${source}/${page.sourcePath}/${filename}`,
-				distDirPath: page.sourcePath
-			}))
-		}))
-	]
-
 	return {
-		pages,
+
+		//
+		// page outputs
+		//
+
+		pages: [
+
+			// home page
+			{
+				id: home.id,
+				name: "home",
+				distPath: "index.html",
+				content: "",
+				files: home.files.map(file => ({
+					fullSourceFilePath: `${source}/${home.sourcePath}/${file}`,
+					distDirPath: ""
+				}))
+			},
+	
+			// blog index
+			{
+				id: blogIndex.id,
+				name: "blog-index",
+				distPath: "blog/index.html",
+				content: "",
+				files: blogIndex.files.map(file => ({
+					fullSourceFilePath: `${source}/${home.sourcePath}/${file}`,
+					distDirPath: ""
+				}))
+			},
+	
+			// blog posts
+			...blogPosts.map(page => ({
+				id: page.id,
+				name: path.basename(page.sourcePath),
+				distPath: page.sourcePath.replace(/^blog\//i, "") + "/index.html",
+				content: "",
+				files: page.files.map(filename => ({
+					fullSourceFilePath: `${source}/${page.sourcePath}/${filename}`,
+					distDirPath: page.sourcePath
+				}))
+			})),
+	
+			// articles
+			...articles.map(page => ({
+				id: page.id,
+				name: path.basename(page.sourcePath),
+				distPath: page.sourcePath.replace(/^articles\//i, "") + "/index.html",
+				content: "",
+				files: page.files.map(filename => ({
+					fullSourceFilePath: `${source}/${page.sourcePath}/${filename}`,
+					distDirPath: page.sourcePath
+				}))
+			}))
+		],
+
+		//
+		// scss stylesheets
+		//
+
 		styles: await Promise.all(
 			websiteMetadata.styles.map(async({data, sourcePath}) => {
 				const sassResult = await sassRender({data, includePaths: [source], fiber})
